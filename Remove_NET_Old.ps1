e0bdf6efb7c4c2bb9e8bf7f414a9e083a41cc4f5
JGMelon22 <jgvasconcellos22@gmail.com>
2023-01-18 19:54:17 -0300

    Adding extra directories

--- Remove_NET_Old.ps1
+++ Remove_NET_Old.ps1
@@ -1,4 +1,4 @@
-$version="6.0.12" # Here you set the SDK versions you wish to remove
+$version="7.*" # Here you set the SDK versions you wish to remove
 
 $Host.UI.RawUI.ForegroundColor = "Yellow"
 
@@ -15,6 +15,42 @@ catch [System.Management.Automation.ItemNotFoundException] { $null }
 'Not Found Directory. Skipping...'
 
 try { Remove-Item -Force -Path "C:\Program Files\dotnet\shared\Microsoft.WindowsDesktop.App\$version" -erroraction stop }
+catch [System.Management.Automation.ItemNotFoundException] { $null }
+'Not Found Directory. Skipping...'
+
+try { Remove-Item -Force -Path "C:\Program Files\dotnet\packs\Microsoft.AspNetCore.App.Ref\$version" -erroraction stop }
+catch [System.Management.Automation.ItemNotFoundException] { $null }
+'Not Found Directory. Skipping...'
+
+try { Remove-Item -Force -Path "C:\Program Files\dotnet\packs\Microsoft.NETCore.App.Host.win-arm\$version" -erroraction stop }
+catch [System.Management.Automation.ItemNotFoundException] { $null }
+'Not Found Directory. Skipping...'
+
+try { Remove-Item -Force -Path "C:\Program Files\dotnet\packs\Microsoft.NETCore.App.Host.win-arm64\$version" -erroraction stop }
+catch [System.Management.Automation.ItemNotFoundException] { $null }
+'Not Found Directory. Skipping...'
+
+try { Remove-Item -Force -Path "C:\Program Files\dotnet\packs\Microsoft.NETCore.App.Host.win-x64\$version" -erroraction stop }
+catch [System.Management.Automation.ItemNotFoundException] { $null }
+'Not Found Directory. Skipping...'
+
+try { Remove-Item -Force -Path "C:\Program Files\dotnet\packs\Microsoft.NETCore.App.Host.win-x86\$version" -erroraction stop }
+catch [System.Management.Automation.ItemNotFoundException] { $null }
+'Not Found Directory. Skipping...'
+
+try { Remove-Item -Force -Path "C:\Program Files\dotnet\packs\Microsoft.NETCore.App.Ref\$version" -erroraction stop }
+catch [System.Management.Automation.ItemNotFoundException] { $null }
+'Not Found Directory. Skipping...'
+
+try { Remove-Item -Force -Path "C:\Program Files\dotnet\packs\Microsoft.WindowsDesktop.App.Ref\$version" -erroraction stop }
+catch [System.Management.Automation.ItemNotFoundException] { $null }
+'Not Found Directory. Skipping...'
+
+try { Remove-Item -Force -Path "C:\Program Files\dotnet\sdk-manifests\$version" -erroraction stop }
+catch [System.Management.Automation.ItemNotFoundException] { $null }
+'Not Found Directory. Skipping...'
+
+try { Remove-Item -Force -Path "C:\Program Files\dotnet\templates\$version" -erroraction stop }
 catch [System.Management.Automation.ItemNotFoundException] { $null }
 'Not Found Directory. Skipping...'
 
